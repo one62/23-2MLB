9fb7c0884785ace17b28ecdeceb067e9d7c30eea
yunhwan <yunhwan.choo00@gmail.com>
2023-11-22 14:53:54 +0900

    conv1D training

--- CNN_mockup.py
+++ CNN_mockup.py
@@ -1,15 +1,22 @@
+import ast
+import pickle
+import numpy as np
 import torch
-
-# 목업 데이터 요소
-num_songs = 500  # 노래 개수
-codes_per_song = 100  # 노래당 코드 개수
-numbers_per_code = 4  # 코드 1개당 표현
-
-#  목업 데이터 생성
-music_codes = torch.rand((num_songs, numbers_per_code, codes_per_song))
+import pandas as pd
+from torch.utils.data import DataLoader, TensorDataset
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-music_codes = music_codes.to(device)
+
+with open('training_input.pkl', 'rb') as f:
+    training_input = pickle.load(f)
+
+training_input = training_input.permute(0, 2, 1)
+training_input = training_input.type(torch.float)
+
+training_output = torch.load('training_output.pkl')
+
+dataset = TensorDataset(training_input, training_output)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 
 class Chord_CNN(torch.nn.Module):
@@ -18,28 +25,96 @@ class Chord_CNN(torch.nn.Module):
 
         # 첫번째 합성곱 계층
         self.conv1 = torch.nn.Sequential(
-            # output shape : [500, 64, 97]
-            # 높이 4의 window -> 100 크기에 1씩 올려가며 적용 -> 97짜리 배열 -> 이짓을 64번
-            torch.nn.Conv1d(in_channels=4, out_channels=64, kernel_size=4, stride=1),
-            # ReLU 함수
+            torch.nn.Conv1d(in_channels=3, out_channels=6, kernel_size=4, stride=1),
+            torch.nn.BatchNorm1d(6),
             torch.nn.ReLU(),
-            # 위의 97짜리 배열에서 윈도 3으로 max poling ->  97이 32됨
-            torch.nn.MaxPool1d(kernel_size=3, stride=3)
+            torch.nn.MaxPool1d(kernel_size=4, stride=4)
+        )
+
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv1d(in_channels=6, out_channels=10, kernel_size=4, stride=1),
+            torch.nn.BatchNorm1d(10),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool1d(kernel_size=4, stride=4)
         )
 
         self.flatten = torch.nn.Flatten()
 
-        self.affine = torch.nn.Linear(64 * 32, 1)
+        self.affine1 = torch.nn.Linear(6 * 49, 950)
+        # 6 * 49
 
     def forward(self, x):
         x = self.conv1(x)
+        # x = self.conv2(x)
         x = self.flatten(x)
-        x = self.affine(x)
+        x = self.affine1(x)
         return x
 
 
 model = Chord_CNN().to(device)
 
-output = model(music_codes)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-print(output.shape)
+num_epochs = 20
+
+for epoch in range(num_epochs):
+    for inputs, targets in dataloader:
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets.float())
+        loss.backward()
+        optimizer.step()
+
+    print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item()}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# training_input = training_input.type(torch.float)
+# training_input = training_input.to(device)
+# output = model(training_input)
+#
+# print(output.shape)
+# print(output)
+
+
+
+
+
+
+
+
+
+# with open('data-6.pkl', 'rb') as f:
+#     data = pickle.load(f)
+#
+# training_output = np.array(data.genres)
+#
+# training_output = [ast.literal_eval(item) for item in training_output]
+#
+# training_output_first = [sublist[0] if sublist else 'none' for sublist in training_output]
+#
+# training_output_first = np.array(training_output_first)
+#
+# print(training_output_first)
+#
+# one_hot_encoded = pd.get_dummies(training_output_first).values.astype(int)
+#
+# print(one_hot_encoded[1])
+#
+# torch.save(torch.tensor(one_hot_encoded), 'training_output.pkl')
